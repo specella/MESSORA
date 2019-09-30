c73396aac4e6ea87a905925fead9eff9034b2a15
specella <annnurb@gmail.com>
2019-09-30 13:28:53 -0400

    add form validations and about page

---
+++ form.js
@@ -0,0 +1,18 @@
+function send {
+	var sendButton = document.querySelector("#submit");
+	sendButton.addEventListener("click", function) {
+	form.reset();
+}
+
+console.log(sendButton);
+}
+
+function ValidateEmail(mail) {
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+		return (true);
+	}
+	alert("You have entered an invalid email address!")
+	return (false)
+}
+
+send();
---
+++ js/form.js
@@ -0,0 +1,48 @@
+var contact = document.querySelector("#send");
+contact.addEventListener("click", function(event) {
+	event.preventDefault();
+
+	var erros = validateForm(contact);
+	console.log(erros);
+
+	if(erros.length > 0) {
+		exibeErro(erros);
+		return;
+	}
+
+	var errorMessage = document.querySelector("#error-message");
+	errorMessage.innerHTML = "";
+
+})
+
+function exibeErro(erros){
+	var ul = document.querySelector("#error-message");
+	ul.innerHTML = "";
+
+	erros.forEach(function(erro){
+		var li = document.createElement("li");
+		li.textContent = erro;
+		ul.appendChild(li);
+	});
+}
+
+function validateForm(contact) {
+
+	var erros = [];
+
+	if (document.forms["contactForm"]["name"].value == "") {
+		erros.push("Name must be filled out");
+	}
+
+	if (document.forms["contactForm"]["email"].value == "") {
+		erros.push("Email must be filled out");
+	}
+
+	if (document.forms["contactForm"]["msg"].value == "") {
+		erros.push("Message cannot be blank");
+	}
+
+	return erros;
+}
+
+
