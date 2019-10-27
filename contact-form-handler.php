5f8c47dd8494b96a57e79c36f2a4703adbdb659c
specella <annnurb@gmail.com>
2019-10-27 19:09:46 -0400

    hooking up send button

---
+++ contact-form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$errors = '';
+$myemail = 'annnurb@gmail.com';
+if(empty($_POST['name']) ||
+	empty($_POST['email']) ||
+	empty($_POST['message']))
+{
+	$errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
