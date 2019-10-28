fc824bb73138281504bc255e9fb87ced28e107e7
specella <annnurb@gmail.com>
2019-10-28 19:30:36 -0400

    adding formspree

--- js/form.js
+++ js/form.js
@@ -1,19 +1,27 @@
 var contact = document.querySelector("#send");
+
 contact.addEventListener("click", function(event) {
 	event.preventDefault();
 
 	var erros = validateForm(contact);
 	console.log(erros);
+	console.log(sendForm)
 
 	if(erros.length > 0) {
 		exibeErro(erros);
 		return;
+	}else if(erros.length = 0) {
+		sendForm();
 	}
 
 	var errorMessage = document.querySelector("#error-message");
 	errorMessage.innerHTML = "";
 
 })
+
+function sendForm(contact) {
+	contact.submit();
+}
 
 function exibeErro(erros){
 	var ul = document.querySelector("#error-message");
